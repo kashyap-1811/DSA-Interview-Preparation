0032afac097e931c3aef5ae71035fcda462e0da8
kashyap-1811 <kashyaprupareliya1811@gmail.com>
2026-01-15 22:09:27 +0530

    Add Binary Trees Codes

--- BinaryTree/Intermediate/BottomView.cpp
+++ BinaryTree/Intermediate/BottomView.cpp
@@ -1,84 +1,42 @@
+// leet - 
+// https://www.geeksforgeeks.org/problems/bottom-view-of-binary-tree/1
+ 
 #include <bits/stdc++.h>
-
 using namespace std;
-
-class Node{
-    public:
+ 
+class Node {
+public:
     int data;
-    Node *left;
-    Node *right;
+    Node* left;
+    Node* right;
 
-    Node(int data)
-    {
-        this->data = data;
-        left = right = nullptr;
-    }
-
-    ~Node(){
-        if(left != nullptr)
-            delete left;
-        if(right != nullptr)
-            delete right;
+    Node(int x) {
+        data = x;
+        left = right = NULL;
     }
 };
 
-static int idx = -1;
-
-Node* buildTree(vector<int>nodes)
-{
-    idx++;
-    if(nodes[idx] == -1)
-        return nullptr;
-
-    Node *new_node = new Node(nodes[idx]);
-    new_node->left = buildTree(nodes);
-    new_node->right = buildTree(nodes);
-
-    return new_node;
-}
-
-void BottomView(Node *root)
-{
-    queue<pair<Node*,int>>q;
-    map<int,int>m;
-
-    q.push(make_pair(root,0));
-
-    while(!q.empty())
-    {
-        pair<Node*,int>curr = q.front();
-        q.pop();
-
-        Node *currNode = curr.first;
-        int currHD = curr.second;
-
-        m[currHD] = currNode->data;
-
-        if(currNode->left != nullptr)
-        {
-            pair<Node*,int>left = make_pair(currNode->left,currHD-1);
-            q.push(left);
-        }
-
-        if(currNode->right != nullptr)
-        {
-            pair<Node*,int>right = make_pair(currNode->right,currHD+1);
-            q.push(right);
-        }
+class Solution {
+  public:
+  
+    void traverse(Node *root, map<int, pair<int, int>> &m, int row, int col){
+        if(root == nullptr)
+            return;
+        
+        if(!m.count(col) || m[col].first <= row)
+            m[col] = {row, root->data};
+        traverse(root->left, m, row+1, col-1);
+        traverse(root->right, m, row+1, col+1);
     }
-
-    for(auto it : m)
-    {
-        cout<<it.second<<" ";
+    
+    vector<int> bottomView(Node *root) {
+        // code here
+        map<int, pair<int, int>> m;
+        traverse(root, m, 0, 0);
+        
+        vector<int> ans;
+        for(auto [_, x] : m)
+            ans.push_back(x.second);
+        return ans;
     }
-}
-
-
-int main()
-{
-    vector<int>nodes = {1,2,3,4,-1,-1,5,-1,6,-1,-1,7,-1,8,-1,-1,9,10,-1,-1,11,-1,12,13,-1,-1,14,-1,15,-1,-1};
-    Node *root = buildTree(nodes);
-
-    BottomView(root);
-    return 0;
-}
+};
--- BinaryTree/Intermediate/Top_View.cpp
+++ BinaryTree/Intermediate/Top_View.cpp
@@ -1,85 +1,42 @@
+// leet - 
+// https://www.geeksforgeeks.org/problems/top-view-of-binary-tree/1
+ 
 #include <bits/stdc++.h>
-
 using namespace std;
-
-class Node{
-    public:
+ 
+class Node {
+  public:
     int data;
-    Node *left;
-    Node *right;
+    Node* left;
+    Node* right;
 
-    Node(int data)
-    {
-        this->data = data;
-        left = right = nullptr;
-    }
-
-    ~Node(){
-        if(left != nullptr)
-            delete left;
-        if(right != nullptr)
-            delete right;
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
     }
 };
 
-static int idx = -1;
-
-Node* buildTree(vector<int>nodes)
-{
-    idx++;
-    if(nodes[idx] == -1)
-        return nullptr;
-
-    Node *new_node = new Node(nodes[idx]);
-    new_node->left = buildTree(nodes);
-    new_node->right = buildTree(nodes);
-
-    return new_node;
-}
-
-void topView(Node *root)
-{
-    queue<pair<Node*,int>>q; //Node pointer and horizontal distance
-    map<int,int>m; //HORIZONTAL DISTANCE, node value
-
-    q.push(make_pair(root,0));
-
-    while(!q.empty())
-    {
-        pair<Node*,int>curr = q.front(); q.pop();
-
-        Node *currNode = curr.first;
-        int currHD = curr.second;
-
-        if(!m.count(currHD))
-        {
-            m[currHD] = currNode->data;
-        }
-
-        if(currNode->left != nullptr)
-        {
-            pair<Node*,int>left = make_pair(currNode->left, currHD-1);
-            q.push(left);
-        }
-
-        if(currNode->right != nullptr)
-        {
-            pair<Node*,int>right = make_pair(currNode->right, currHD+1);
-            q.push(right);
-        }
+class Solution {
+  public:
+    void traverse(Node *root, map<int, pair<int, int>> &m, int row, int col){
+        if(root == nullptr)
+            return;
+        
+        if(!m.count(col) || m[col].first > row)
+            m[col] = {row, root->data};
+        traverse(root->left, m, row+1, col-1);
+        traverse(root->right, m, row+1, col+1);
     }
-
-    for(auto it : m)
-    {
-        cout<<it.second<<" ";
+    
+    vector<int> topView(Node *root) {
+        // code here
+        map<int, pair<int, int>> m;
+        traverse(root, m, 0, 0);
+        
+        vector<int> ans;
+        for(auto [_, x] : m)
+            ans.push_back(x.second);
+        return ans;
     }
-}
-
-int main()
-{
-    vector<int>nodes = {1,2,3,4,-1,-1,5,-1,6,-1,-1,7,-1,8,-1,-1,9,10,-1,-1,11,-1,12,13,-1,-1,14,-1,15,-1,-1};
-    Node *root = buildTree(nodes);
-
-    topView(root);
-    return 0;
-}
+};
